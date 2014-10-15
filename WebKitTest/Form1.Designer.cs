1309762a0e45a274717b1c568e3c65defa67af4d
Alexorleon <alexorleon@gmail.com>
2014-10-15 13:28:40 +0700

    create repository

---
+++ WebKitTest/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace WebKitTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webKitBrowser1 = new WebKit.WebKitBrowser();
+            this.SuspendLayout();
+            // 
+            // webKitBrowser1
+            // 
+            this.webKitBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.webKitBrowser1.BackColor = System.Drawing.Color.White;
+            this.webKitBrowser1.Location = new System.Drawing.Point(0, -1);
+            this.webKitBrowser1.Name = "webKitBrowser1";
+            this.webKitBrowser1.Size = new System.Drawing.Size(1010, 731);
+            this.webKitBrowser1.TabIndex = 1;
+            this.webKitBrowser1.Url = null;
+            this.webKitBrowser1.Load += new System.EventHandler(this.webKitBrowser1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 730);
+            this.Controls.Add(this.webKitBrowser1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private WebKit.WebKitBrowser webKitBrowser1;
+    }
+}
+
---
+++ WebKitTest/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime;
+using System.Runtime.InteropServices;
+
+namespace WebKitTest
+{
+    public partial class Form1 : Form
+    {
+        static DateTime current_time = DateTime.Now; // текущее время
+        private readonly Timer tmrShow; // таймер для периодической проверки соединения
+
+        // проверка интернет соединения
+        [DllImport("wininet.dll")]
+        private extern static bool InternetGetConnectedState(out int Description, int ReservedValue);
+
+        private bool isFirst = true; // чтобы лишний раз не дергать страницу
+
+        public Form1()
+        {
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+            TopMost = true;
+
+            Cursor.Hide();
+
+            tmrShow = new Timer(); // создаем новый таймер
+            tmrShow.Interval = 5000; // ставим интервал выполнения единственного события, через 5 секунд
+            tmrShow.Tick += check_link; // создаем событие
+            tmrShow.Enabled = true;// включаем таймер
+        }
+
+        private void txtBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBox_KeyDown(object sender, KeyEventArgs e)
+        {
+        }
+
+        private void txtBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            /*if (e.KeyChar == '\r')
+            {
+                webKitBrowser1.Navigate(txtBox.Text);
+            }*/
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webKitBrowser1_Load(object sender, EventArgs e)
+        {
+            //int counter = 0;
+            
+            // Read the file and display it line by line.
+            System.IO.StreamReader file = new System.IO.StreamReader(Environment.CurrentDirectory + "\\files\\address.txt");
+            string line = file.ReadLine();
+            /*while ((line = file.ReadLine()) != null)
+            {
+            }*/
+
+            file.Close();
+            webKitBrowser1.Navigate(line);
+        }
+
+        // проверяем сеть
+        private void check_link(object sender, EventArgs e)
+        {
+            //bool status = System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
+            //MessageBox.Show("status= " + status);
+
+            int Desc;
+            bool statusConnect = InternetGetConnectedState(out Desc, 0);
+            //MessageBox.Show(statusConnect.ToString());
+            
+            // если нет связи, показываем нашу страницу
+            if (!statusConnect)
+            {
+                if (isFirst)
+                {
+                    string strpath = ("file:///" + Environment.CurrentDirectory.Replace(@"\", @"/") + "/files/106.html");
+                    webKitBrowser1.Navigate(strpath.Replace(@" ", @"%20"));
+                    isFirst = false;
+                }
+            }
+            else
+            {
+                if ( !isFirst)
+                {
+                    System.IO.StreamReader file = new System.IO.StreamReader(Environment.CurrentDirectory + "\\files\\address.txt");
+                    string line = file.ReadLine();
+
+                    file.Close();
+                    webKitBrowser1.Navigate(line);
+                    isFirst = true;
+                }
+            }
+        }
+    }
+}
